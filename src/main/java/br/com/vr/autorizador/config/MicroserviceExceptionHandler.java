e25055955c7a7870789e73e8281d6e3381f71636
Weslley Matos de Carvalho <contato@matcarv.com>
2024-11-03 07:45:27 -0300

    Correção de erros

--- src/main/java/br/com/vr/autorizador/config/MicroserviceExceptionHandler.java
+++ src/main/java/br/com/vr/autorizador/config/MicroserviceExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.vr.autorizador.config;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -23,11 +24,30 @@ public class MicroserviceExceptionHandler {
 	 * @param request
 	 * @return
 	 */
-	@ExceptionHandler({ NotFoundBusinessException.class, UnprocessableBusinessException.class })
+	@ExceptionHandler({
+		NotFoundBusinessException.class, 
+		UnprocessableBusinessException.class })
     public final ResponseEntity<?> handleException(final ResponseStatusException ex, final WebRequest request) {
 		return ResponseEntity
                 .status(ex.getStatusCode())
                 .body(ex.getReason());
 	}
+	
+	/**
+	 * 
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler({ MethodArgumentNotValidException.class })
+	public ResponseEntity<?> handleException(final MethodArgumentNotValidException ex) {
+		return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(ex.getBindingResult()
+                		.getAllErrors()
+                		.stream().map(e -> { 
+                			return e.getDefaultMessage(); 
+                		})
+                	);
+	 }
 	
 }
