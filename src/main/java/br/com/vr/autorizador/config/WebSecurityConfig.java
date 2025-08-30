52d2517d255271e1eac1a94483791b3e23da7675
Weslley Matos de Carvalho <contato@matcarv.com>
2025-08-30 17:09:36 -0300

    Atualiza workflow CodeQL

--- src/main/java/br/com/vr/autorizador/config/WebSecurityConfig.java
+++ src/main/java/br/com/vr/autorizador/config/WebSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * 
@@ -48,39 +47,20 @@ public class WebSecurityConfig {
 	 */
 	@Bean
     protected SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.ignoringRequestMatchers(
-                        AntPathRequestMatcher.antMatcher("/"),
-                        AntPathRequestMatcher.antMatcher("/docs"),
-                        AntPathRequestMatcher.antMatcher("/v2/api-docs/**"),
-                        AntPathRequestMatcher.antMatcher("/webjars/**"),
-                        AntPathRequestMatcher.antMatcher("/swagger-resources/**"),
-                        AntPathRequestMatcher.antMatcher("/configuration/**"),
-                        AntPathRequestMatcher.antMatcher("/swagger-ui/**"),
-                        AntPathRequestMatcher.antMatcher("/*.html"),
-                        AntPathRequestMatcher.antMatcher("/favicon.ico"),
-                        AntPathRequestMatcher.antMatcher("/**/*.html"),
-                        AntPathRequestMatcher.antMatcher("/**/*.css"),
-                        AntPathRequestMatcher.antMatcher("/**/*.js")
-                )).csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(
-                                        "/v2/api-docs/**",
-                                        "/swagger-ui/**",
-                                        "/swagger-resources/**",
-                                        "/webjars/**",
-                                        "/configuration/**",
-                                        "/",
-                                        "/*.html",
-                                        "/favicon.ico",
-                                        "/**/*.html",
-                                        "/**/*.css",
-                                        "/**/*.js",
-                                        "/docs"
-                                ).permitAll()
-                                .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/v2/api-docs/**", 
+                "/swagger-ui/**", 
+                "/swagger-resources/**",
+                "/webjars/**", 
+                "/configuration/**", 
+                "/", 
+                "/docs"
+            ).permitAll()
+            .anyRequest().authenticated()
+        )
+        .httpBasic(Customizer.withDefaults());
+
 
 		return http.build();
 	}
